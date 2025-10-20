1ed172941818fa026728098232a4aae57281c2db
DevJihwan <108052941+DevJihwan@users.noreply.github.com>
2025-10-20 23:28:17 +0900

    UnlockAttempt fetchRequest 충돌 문제 해결

--- SmartLockBox/Models/UnlockAttempt+CoreDataExtensions.swift
+++ SmartLockBox/Models/UnlockAttempt+CoreDataExtensions.swift
@@ -10,8 +10,7 @@ import CoreData
 
 // 확장을 통한 fetchRequest 정의로 중복 선언 문제 해결
 extension UnlockAttempt {
-    // fetchRequest 메서드가 CoreDataGenerated 파일에서 자동 생성되는데, 
-    // 이를 확장으로 명시적 재정의하여 충돌 방지
+    // fetchRequest 메서드명을 createFetchRequest()로 변경하여 충돌 방지
     @nonobjc public class func createFetchRequest() -> NSFetchRequest<UnlockAttempt> {
         return NSFetchRequest<UnlockAttempt>(entityName: "UnlockAttempt")
     }
