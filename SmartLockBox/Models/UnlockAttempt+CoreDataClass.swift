aeee9855c4e889bc5205547925f9bbb3c95d2f98
DevJihwan <108052941+DevJihwan@users.noreply.github.com>
2025-10-30 23:07:59 +0900

    Add missing Core Data entity class files for UnlockAttempt and UsageRecord

---
+++ SmartLockBox/Models/UnlockAttempt+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UnlockAttempt+CoreDataClass.swift
+//  SmartLockBox
+//
+//  Created by DevJihwan on 2025-10-10.
+//
+
+import Foundation
+import CoreData
+
+@objc(UnlockAttempt)
+public class UnlockAttempt: NSManagedObject {
+    // Core Data entity class for UnlockAttempt
+    // Properties are defined in UnlockAttempt+CoreDataProperties.swift
+}
---
+++ SmartLockBox/Models/UsageRecord+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UsageRecord+CoreDataClass.swift
+//  SmartLockBox
+//
+//  Created by DevJihwan on 2025-10-10.
+//
+
+import Foundation
+import CoreData
+
+@objc(UsageRecord)
+public class UsageRecord: NSManagedObject {
+    // Core Data entity class for UsageRecord
+    // Properties are defined in UsageRecord+CoreDataProperties.swift
+}
